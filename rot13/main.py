a1eca3b854ce73550fee727cd79e145584fd1d78
Hao Wu <tohaowu@gmail.com>
2015-09-30 07:33:17 -0700

    just make it green

---
+++ rot13/main.py
@@ -0,0 +1,35 @@
+import os
+import re
+from string import letters
+
+import webapp2
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+class Rot13(BaseHandler):
+    def get(self):
+        self.render('rot13-form.html')
+
+    def post(self):
+        rot13 = ''
+        text = self.request.get('text')
+        if text:
+            rot13 = text.encode('rot13')
+
+        self.render('rot13-form.html', text = rot13)
+
+app = webapp2.WSGIApplication([('/unit2/rot13', Rot13)], 
+                              debug=True)
